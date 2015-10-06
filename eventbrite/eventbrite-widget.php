9a00eeaa828b01b50160715a8f0bf2d9cef7791b
Patrick Fung <patrick@makestuffdostuff.com>
2015-10-06 10:38:38 -0400

    embed widget styles, add linkout to eventbrite url

--- eventbrite/eventbrite-widget.php
+++ eventbrite/eventbrite-widget.php
@@ -4,6 +4,29 @@
  */
 ?>
 
+<style type="text/css">
+  /* Events stuff, make this more extensible */
+  .events ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  .events ul li {
+    background: rgba(91,194,244,0.05);
+    border: 2px solid rgb(91,194,244);
+  }
+  .events ul li + li {
+    margin-top: 0.5em;
+  }
+  .events li > a {
+    display: block;
+    padding: 0.5em 1em;
+    width: 100%;
+    height: 100%;
+    text-decoration: none;
+  }
+</style>
+
 <?php
   $events = array_slice( eventbrite_get_events()->events, 0, 5 );
 ?>
@@ -19,9 +42,10 @@
       <?php foreach($events as $event) : ?>
         <?php 
           $event_name = $event->post_title;
-          $event_start = esc_html( mysql2date( 'F j Y @ g:i A', $event->start->local ) )
+          $event_start = esc_html( mysql2date( 'F j Y @ g:i A', $event->start->local ) );
+          $event_url = $event->url;
         ?>
-        <li><?php echo "{$event_name}<br>{$event_start}" ?></li>
+        <li><a href="<?php echo $event_url ?>"><?php echo "{$event_name}<br>{$event_start}" ?></a></li>
       <?php endforeach; ?>
     <?php else : ?>
       <li>No upcoming events!</li>
