2dfb17c4fea2cd9e016178e3344f537f685f7edd
Patrick Fung <patrick@makestuffdostuff.com>
2015-10-27 12:42:32 -0400

    move custom widgets into its own stylesheet and include it

--- functions.php
+++ functions.php
@@ -182,6 +182,8 @@ function underscores_scripts() {
 
 	wp_enqueue_style( 'underscores-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'custom-widget-css', get_template_directory_uri() . '/css/custom_widgets.css' );
+
 	wp_enqueue_script( 'underscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'underscores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
