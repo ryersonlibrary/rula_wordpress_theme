a2e6dd544f2a3249551ad734c7414338ae6f7b38
Patrick Fung <patrick@makestuffdostuff.com>
2015-11-03 11:43:55 -0500

    basic rula-team-single template

--- rula-partials/team-single.php
+++ rula-partials/team-single.php
@@ -9,6 +9,7 @@
 
 ?>
 
+<?php /*
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <header class="entry-header">
     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
@@ -29,4 +30,41 @@
     <?php underscores_entry_footer(); ?>
   </footer><!-- .entry-footer -->
 </article><!-- #post-## -->
+*/ ?>
 
+<style type="text/css">
+  .single-rula-team .hero {
+    position: relative;
+    margin-bottom: 2.25em;
+    max-width: 500px;
+  }
+
+  .single-rula-team .hero h1 {
+    background: #323132;
+    color: #F1F1F2;
+    position: absolute;
+    bottom: -0.75em;
+    right: -0.75em;
+    margin: 0;
+    padding: 0.5em;
+    line-height: 1;
+  }
+
+  @media screen and (min-width: 34.250em) {
+    .single-rula-team .hero {
+      margin-right: 3em;
+      max-width: 50%;
+      float: left;
+    }
+  }
+</style>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="hero">
+    <?php the_post_thumbnail('rula-team-image'); ?>
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+  </div>
+  <div class="entry-content">
+    <?php the_content(); ?>
+  </div>
+</article>
